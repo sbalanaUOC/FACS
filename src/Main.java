7af8332db62b73e94ab3436c85bc5ff36b64c106
sbalanaUOC <sbalana@uoc.edu>
2025-10-20 00:05:32 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// test cambio
+
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
