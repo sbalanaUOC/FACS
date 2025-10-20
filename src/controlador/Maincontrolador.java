7381c97ff94cc6c1618c7c87ae47ee063b0feef5
sbalanaUOC <sbalana@uoc.edu>
2025-10-20 23:43:31 +0200

    test de commit 1/1

--- src/controlador/Maincontrolador.java
+++ src/controlador/Maincontrolador.java
@@ -6,10 +6,43 @@ import vista.Mainvista;
 public class Maincontrolador {
     private Mainmodelo modelo;
     private Mainvista vista;
+
+
     public Maincontrolador(Mainmodelo inmodelo, Mainvista invista) {
         this.modelo = inmodelo;
         this.vista = invista;
     }
+
+
     // Métodos que conectan vista y modelo
 
+    public void iniciarPrograma() {
+        int opcion;
+
+        do {
+            opcion = vista.mostrarMenuPrincipal();
+            procesarOpcionPrincipal(opcion);
+        } while (opcion != 0);
+    }
+
+    private void procesarOpcionPrincipal(int opcion) {
+        switch (opcion) {
+            case 1:
+                vista.mostrarMenuArticulos();
+                break;
+            case 2:
+                vista.mostrarMenuClientes();
+                break;
+            case 3:
+                vista.mostrarMenuPedidos();
+                break;
+            case 0:
+                System.out.println("Fin del programa.");
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                break;
+        }
+
+    }
 }
--- src/modelo/Mainmodelo.java
+++ src/modelo/Mainmodelo.java
@@ -1,4 +1,18 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Mainmodelo {
+    private OnlineStore store;
+
+
+
+    public Mainmodelo(OnlineStore store) {this.store = store;}
+
+
+
+    public OnlineStore getStore() {return store;}
+    public void setStore(OnlineStore store) {this.store = store;}
+
+
 }
